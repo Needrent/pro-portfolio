4c11711b54d4636af55b6c92b538f7f8910ec3a5
SEJ <sej@needrent.dk>
2019-10-22 11:34:45 +0200

    init base and repo

---
+++ javascript/index.js
@@ -0,0 +1,19 @@
+/*console.log(
+  "%cHello fellow developer %cThe answear is... %cYes! %cYes Im open for hire!",
+  "padding: 0 2px 0 2px; font-size: 18px; background-color: black; color: white; line-height: 25px",
+  "padding: 0 2px 0 2px; font-size: 18px; background-color: red; color: white; line-height: 25px",
+  "padding: 0 2px 0 2px; font-size: 18px; background-color: green; color: white; line-height: 25px",
+  "padding: 0 2px 0 2px; font-size: 18px; background-color: blue; color: white; line-height: 25px"
+);*/
+console.log(
+  "%cHello fellow developer",
+  "padding: 0 2px 0 2px; font-size: 18px; background-color: black; color: white; line-height: 25px"
+);
+console.log(
+  "%cYes Im open for hire!",
+  "padding: 0 2px 0 2px; font-size: 18px; background-color: red; color: white; line-height: 25px"
+);
+console.log(
+  "%csej@needrent.dk",
+  "padding: 0 2px 0 2px; font-size: 18px; background-color: red; color: white; line-height: 25px"
+);
