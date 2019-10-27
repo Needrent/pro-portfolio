428310673f2259d988424a11d13b43b90d12c4d7
SEJ <sej@needrent.dk>
2019-10-27 18:27:05 +0100

    mobile layout coded

--- javascript/index.js
+++ javascript/index.js
@@ -3,13 +3,6 @@ window.addEventListener("load", () => {
   vidCoffee.play();
 });
 
-/*console.log(
-  "%cHello fellow developer %cThe answear is... %cYes! %cYes Im open for hire!",
-  "padding: 0 2px 0 2px; font-size: 18px; background-color: black; color: white; line-height: 25px",
-  "padding: 0 2px 0 2px; font-size: 18px; background-color: red; color: white; line-height: 25px",
-  "padding: 0 2px 0 2px; font-size: 18px; background-color: green; color: white; line-height: 25px",
-  "padding: 0 2px 0 2px; font-size: 18px; background-color: blue; color: white; line-height: 25px"
-);*/
 console.log(
   "%cHello fellow developer",
   "padding: 0 2px 0 2px; font-size: 18px; background-color: black; color: white; line-height: 25px"
